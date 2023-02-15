fa782797a41c373beff7b39fe6a040c14b8bb5e0
Ethan MIS <emis@e1r6p4.clusters.42paris.fr>
2023-02-15 14:46:44 +0100

    Better testing, works differently/slower for bonus since semaphores can intersect between the processes.

--- philotester.sh
+++ philotester.sh
@@ -3,7 +3,7 @@
 getphiloprocess(){
 	p=$(ps aux | grep '[p]hilo' | grep " $1 $2 $3 $4" | awk '{print $2}')
 	# echo $p > /dev/stderr
-	if [ -n "$p" ];then kill $p;fi
+	if [ -n "$p" ] && [ -n "$1" ];then kill $p;fi
 	echo $p
 	exit 0
 }
@@ -23,55 +23,76 @@ shouldnotstop(){
 	fi
 }
 
+shoulddie(){
+	(./philo $1 $2 $3 $4 $5 > ./$dir/shd_$1_$2_$3_$4_$5) &
+	s=$(( ( $2 / 1000 ) + 2))
+	sleep "$s"s
+	p=$(getphiloprocess $1 $2 $3 $4 $5)
+	e=$(cat ./$dir/shd_$1_$2_$3_$4_$5 | grep died | wc -l)
+	if [ -n "$p" ] || [ $e -ne 1 ]; 
+	then echo -e "$1 $2 $3 $4 $5 :$RED fail (did not stop or died philo != 1)$NC";
+	else echo -e "$1 $2 $3 $4 $5 :$GREEN success (1 died)$NC";
+	fi
+}
+
 shouldstop(){
 	(./philo $1 $2 $3 $4 $5 > ./$dir/shst_$1_$2_$3_$4_$5) &
-	sleep 5s
+	s=$((( ( ( 2 + ($1 % 2) ) * $3 * $5 ) / 1000 ) + 2))
+	sleep "$s"s
 	p=$(getphiloprocess $1 $2 $3 $4 $5)
-	if [ -n "$p" ]; 
-	then echo -e "$1 $2 $3 $4 $5 :$RED fail (did not stop)$NC";
+	e=$(cat ./$dir/shst_$1_$2_$3_$4_$5 | grep died | wc -l)
+	if [ -n "$p" ] || [ $e -ne 0 ]; 
+	then echo -e "$1 $2 $3 $4 $5 :$RED fail (did not stop or died)$NC";
 	else echo -e "$1 $2 $3 $4 $5 :$GREEN success (stopped)$NC";
 	fi
 }
 
 shoulderror(){
-	(./philo $1 $2 $3 $4 $5 2> ./$dir/err_$1_$2_$3_$4_$5) &
+	(./philo $@ 2> ./$dir/err_$1_$2_$3_$4_$5) &
 	sleep 1s
 	p=$(getphiloprocess $1 $2 $3 $4 $5)
 	e=$(cat ./$dir/err_$1_$2_$3_$4_$5)
-	if [ -n "$p" ] || [ -z "$e" ];
-	then echo -e "$1 $2 $3 $4 $5 :$RED fail (no error)$NC";
-	else echo -e "$1 $2 $3 $4 $5 :$GREEN success (error)$NC";
+	if [ -z "$e" ];
+	then echo -e "$@ :$RED fail (no error)$NC";
+	else echo -e "$@ :$GREEN success (error)$NC";
 	fi
 }
 
 runtests(){
+	cd ./philo$bonus
+	make && make clean
 	$(mkdir -p $dir)
-	(shouldnotstop 5 800 200 200) &
-	(shouldnotstop 5 600 150 150) &
-	(shouldnotstop 4 410 200 200) &
-	(shouldnotstop 100 800 200 200) &
-	(shouldnotstop 105 800 200 200) &
-	(shouldnotstop 200 800 200 200) &
-	# wait
-	(shouldstop 1 800 200 200) &
-	(shouldstop 4 310 200 100) &
-	(shouldstop 4 200 205 200) &
-	(shouldstop 5 800 201 200 7) & # having tests with same arguments 1 2 3 4 can yield unwanted results.
-	(shouldstop 4 410 201 200 10) &
-	# wait
-	(shoulderror -5 600 200 200) &
-	(shoulderror 4 -5 200 200) &
-	(shoulderror 4 600 -5 200) &
-	(shoulderror 4 600 200 -5) &
-	(shoulderror 4 600 200 200 -5) &
-	(shoulderror 0 0 0 0 0) &
+	for n in "-5 600 200 200" "4 -5 200 200" "4 600 -5 200" "4 600 200 -5" "4 600 200 200 -5" "0 0 0 0 0" "" "10" "10 10" "10 10 10" "10 10 10 10 10 10"
+	do
+		(shoulderror $n) &
+		if [ -n "$1" ]; then wait; fi
+	done
+	for n in "5 800 200 200" "5 600 150 150" "4 410 200 200" "100 800 200 200" "105 800 200 200" "200 800 200 200"
+	do
+		(shouldnotstop $n) &
+		if [ -n "$1" ]; then wait; fi
+	done
+	for n in "1 800 200 200" "4 310 200 100" "4 200 205 200"
+	do
+		(shoulddie $n) &
+		if [ -n "$1" ]; then wait; fi
+	done
+	for n in "5 800 201 200 7" "4 410 201 200 10"
+	do
+		(shouldstop $n) &
+		if [ -n "$1" ]; then wait; fi
+	done
 	wait
 	echo "Testing complete !"
 	rm -rf $dir
+	cd ..
 	exit 0
 }
 
-runtests
+if [ "$1" = "bonus" ];
+then bonus="_bonus"; runtests bonus;
+else bonus=""; runtests;
+fi
 
 # 5 800 200 200
 # no one should die
